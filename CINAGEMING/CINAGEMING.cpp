622e53cd3247553c01d817352acdb482b80b0367
voltarou <bagusadnan04@gmail.com>
2024-03-29 10:15:26 +0800

    void input

--- CINAGEMING/CINAGEMING.cpp
+++ CINAGEMING/CINAGEMING.cpp
@@ -8,6 +8,24 @@ int n;
 
 void input() {
     while (true)
+    {
+        cout << "Masukan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+    cout << "\n----------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n----------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
 }
 
 int main()
