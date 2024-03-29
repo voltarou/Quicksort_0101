9fb9236f745ca0ce2c5ff78b26cdcbb7fcc79de4
voltarou <bagusadnan04@gmail.com>
2024-03-29 10:50:41 +0800

    cout

--- CINAGEMING/CINAGEMING.cpp
+++ CINAGEMING/CINAGEMING.cpp
@@ -93,7 +93,7 @@ void display() {
         cout << arr[i] << " ";
     }
 
-    cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+    cout << "\n\nNumber of comparasions: " << cmp_count << endl; 
     cout << "Number of data movements: " << mov_count << endl;
 }
 
