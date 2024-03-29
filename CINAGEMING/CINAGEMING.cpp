7783015aad986dfa7d92f5c8a70ff46237179a03
voltarou <bagusadnan04@gmail.com>
2024-03-29 10:41:19 +0800

    void display

--- CINAGEMING/CINAGEMING.cpp
+++ CINAGEMING/CINAGEMING.cpp
@@ -83,6 +83,20 @@ void q_short(int low, int high)
     q_short(j + 1, high);
 }
 
+void display() {
+    cout << "\n----------------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "----------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
+}
+
 
 int main()
 {
