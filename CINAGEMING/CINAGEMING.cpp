ddcdde15696f22fc5bb70b7365143f77855631d7
voltarou <bagusadnan04@gmail.com>
2024-03-29 10:46:36 +0800

    int main

--- CINAGEMING/CINAGEMING.cpp
+++ CINAGEMING/CINAGEMING.cpp
@@ -100,6 +100,22 @@ void display() {
 
 int main()
 {
+    char ch;
+
+    do {
+        input();
+        q_short(0, n - 1);
+        display();
+        cout << "\n\nDo you want to continue? (y/n): ";
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+            break;
+
+        system("pause");
+        system("cls");
+    } while (true);
+
+    return 0;
     
 }
 
